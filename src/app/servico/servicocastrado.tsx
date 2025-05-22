692b68f8ca9f209a9fa314671c3439b992807b7d
Odiegodev1 <odiegodev10@gmail.com>
2025-05-21 21:53:33 -0300

    Mensagem explicando as mudanças

--- src/app/servico/servicocastrado.tsx
+++ src/app/servico/servicocastrado.tsx
@@ -11,8 +11,8 @@ export default async function ServicoCadastrado() {
   const { createClient } = await import('@supabase/supabase-js')
 
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 
   const { data: produtos, error } = await supabase.from('produtos').select('*')
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Variáveis de ambiente do Supabase não foram definidas.')
+}
+
 export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY! // Use a chave secreta só no servidor!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
